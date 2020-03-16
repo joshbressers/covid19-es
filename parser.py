60cf889f5ef90cbd520d79ee62e9b5ff54848647
Josh Bressers <josh@bress.net>
2020-03-16 17:47:54 -0500

    Fix the timestamps in the last_updated field

--- parser.py
+++ parser.py
@@ -4,9 +4,14 @@ import os
 import json
 import glob
 import csv
+import re
 import elasticsearch
 import elasticsearch.helpers
 from elasticsearch import Elasticsearch
+
+# Dates are hard
+last_year4 = re.compile(r'(\d+)\/(\d+)\/(\d{4}) (\d+)\:(\d+)')
+last_year2 = re.compile(r'(\d+)\/(\d+)\/(\d{2}) (\d+)\:(\d+)')
 
 if 'ESURL' not in os.environ:
     es_url = "http://localhost:9200"
@@ -46,20 +51,42 @@ for f in csv_data:
             # Some of the data is strange. Since we're importing it, we'll
             # do some transforms here
 
+            # Country Mangling
             if i[1] == "Mainland China":
                 i[1] = "China"
-
-            if i[1] == "South Korea":
-                i[1] = "Korea, South"
-
-            if i[1] == "Iran (Islamic Republic of)":
+            elif i[1] == "Korea, South":
+                i[1] = "South Korea"
+            elif i[1] == "Iran (Islamic Republic of)":
                 i[1] = "Iran"
+            elif i[1] == "US":
+                i[1] = "United States"
 
             base["province"] = i[0]
             base["country"] = i[1]
 
-            # This field is a tire fire in the data, we'll fix it later
-            #base["last_update"] = i[2]
+            # Some of the last_update timestamps are broken
+            match4 = last_year4.match(i[2])
+            match2 = last_year2.match(i[2])
+            if match4 is not None:
+                month = int(match4[1])
+                day = int(match4[2])
+                year = int(match4[3])
+                hour = int(match4[4])
+                minute = int(match4[5])
+                new_date = "%02d-%02d-%02dT%02d:%02d:00" % \
+                          (year, month, day, hour, minute)
+                base["last_update"] = new_date
+            elif match2 is not None:
+                month = int(match2[1])
+                day = int(match2[2])
+                year = int(match2[3] + '20')
+                hour = int(match2[4])
+                minute = int(match2[5])
+                new_date = "%02d-%02d-%02dT%02d:%02d:00" % \
+                          (year, month, day, hour, minute)
+                base["last_update"] = new_date
+            else:
+                base["last_update"] = i[2]
 
 
             # Empty strings exist, make them zero
